19d0a915efb140aaea06e358c47bb5d490a987e0
dnam <donghee.nam@anu.edu.au>
2020-04-01 13:49:58 +1100

    update

--- mp_plot.py
+++ mp_plot.py
@@ -30,8 +30,10 @@ def plot_imf(filename, ax, shift_x=1, shift_y=1, binned=True, **kwargs) :
     IMF_nonzero = IMF_binned[IMF_binned!=0]
     M_nonzero = M_binned[IMF_binned!=0]
 
+    # normalise the IMF
+    M_nonzero = M_nonzero * shift_x
+
     # plot the IMF
-    M_nonzero = M_nonzero*shift_x
     ax.plot(M_nonzero, M_nonzero*IMF_nonzero*shift_y, **kwargs)
 
 if __name__ == "__main__" :
@@ -43,32 +45,31 @@ if __name__ == "__main__" :
     # set up the axes
     ax.set_xscale("log")
     ax.set_yscale("log")
-    #ax.set_xlim(left=1e-4, right=1e5)
-    #ax.set_ylim(bottom=1e1, top=1e6)
+    ax.set_xlim(left=1e-8, right=1e2)
+    ax.set_ylim(bottom=1e-4, top=1e8)
 
-    ax.set_xlabel(r"$M[M_\mathrm{sonic}]$")
+    ax.set_xlabel(r"$M[\rho_0 h^3]$")
     ax.set_ylabel(r"$\dfrac{dN}{d\log M}$")
-    #ax.set_title(r'$E_v\propto k^{-2}, h=1$')
 
     # Salpeter slope
-    x_Sal = np.linspace(1e1,1e5,10)
-    ax.plot(x_Sal, 1e4*(x_Sal/100)**(-1.35), color='orange', linewidth=5, label='Salpeter')
+    x_Sal = np.linspace(1e-4,1e0,10)
+    ax.plot(x_Sal, x_Sal**(-1.35), color='orange', linewidth=3, ls='--', label='Salpeter')
 
     # plot the IMFs
-    plot_imf("result/M30p2B0.5n800.hdf5", ax, color='r', label=r'$p=2, \mathcal{M}_{h,1D}=30,\beta=0.5$')
-    plot_imf("M30p2B0.0n5000_dir.hdf5", ax, shift_x=1/1.096026e34, shift_y=1e97, color='r', ls='--', binned=False)
-    plot_imf("result/M30p1.7B0.5n800.hdf5", ax, color='g', label=r'$p=5/3, \mathcal{M}_{h,1D}=30,\beta=0.5$')
-    #plot_imf("M30p1.7B0.0n5000_dir.hdf5", ax, shift_x=1/3.653419e32, shift_y=1e97, color='g', ls='--', binned=False)
-    plot_imf("result/M10p2B0.5n800.hdf5", ax, color='b', label=r'$p=2, \mathcal{M}_{h,1D}=10,\beta=0.5$')
-    #plot_imf("M10p2B0.0n5000_dir.hdf5", ax, shift_x=1/9.864231e34, shift_y=1e97, color='b', ls='--', binned=False)
-    #plot_imf("M30p2B0.5n_var.hdf5", ax, shift_x=4e0, shift_y=1.2e16, label='direct, newmesh')
+    #plot_imf("M1p2B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='--', binned=False, label=r'$\mathcal{M}_h=1,p=2,B=0$')
+    #plot_imf("M3p2B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='--', binned=False, label=r'$\mathcal{M}_h=3,p=2,B=0$')
+    plot_imf("M10p1.0B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='-', color='pink', lw=3, binned=False, label=r'$\mathcal{M}_h=10,p=1,B=0$')
+    plot_imf("M10p1.3B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='-', binned=False, label=r'$\mathcal{M}_h=10,p=4/3,B=0$')
+    plot_imf("M10p1.7B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='-', binned=False, label=r'$\mathcal{M}_h=10,p=5/3,B=0$')
+    plot_imf("M10p2B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='-', color='k', lw=3, binned=False, label=r'$\mathcal{M}_h=10,p=2,B=0$')
+    plot_imf("M10p2.5B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='-', binned=False, label=r'$\mathcal{M}_h=10,p=5/2,B=0$')
+    #plot_imf("M30p2B0.0n5000_dir.hdf5", ax, shift_x=1/3.521e40, shift_y=1e105, ls='--', binned=False, label=r'$\mathcal{M}_h=30,p=2,B=0$')
 
     # data from Hopkins (2012)
-    m30x, m30y, m10x, m10y = np.power(10, np.genfromtxt(
-        "Hopkins2012.csv", delimiter=',', skip_header=2, unpack=True))
-
+    #m30x, m30y, m10x, m10y = np.power(10, np.genfromtxt(
+    #    "Hopkins2012.csv", delimiter=',', skip_header=2, unpack=True))
     #ax.plot(m10x, m10y, color='k', linestyle='--')
-    ax.plot(m30x, m30y, color='k', linewidth=2, label='Hopkins(2012)')
+    #ax.plot(m30x, m30y, color='k', linewidth=2, label='Hopkins(2012)')
 
     # save the plot
     plt.legend(prop={'size':12}, loc='lower left')
